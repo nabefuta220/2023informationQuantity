ce642b2a6a255dcccddcd21c821ff8ea89e2dcab
Watanabe Satoshi <satoshinabe220@gmail.com>
2024-01-23 15:01:40 +0900

    テストケースを移動

--- s4/B233375/TestCase.java
+++ s4/B233375/TestCase.java
@@ -66,6 +66,21 @@ public class TestCase {
 				freq = noSpace.frequency();
 				assert freq == 0 : "Test Case 6 Failed: Space not set";
 			}
+			testFrequencerWithRange("3210321001230123", "3210321001230123", 0, 16, 1);// 全範囲
+			testFrequencerWithRange("3210321001230123", "3210321001230123", 3, 4, 4);// 範囲が1
+			testFrequencerWithRange("3210321001230123", "3210321001230123", 3, 5, 1);// 範囲が2
+			testFrequencerWithRange("3210321001230123", "3210321001230123", 1, 3, 2);// 範囲が2
+			// slow/TestCaseから拝借
+			testFrequencer("AAA", "", -1);
+			testFrequencer("", "A", 0);
+			testFrequencer("AAA", "A", 3);
+			testFrequencer("AAA", "AA", 2);
+			testFrequencer("AAA", "AAA", 1);
+			testFrequencer("AAA", "AAAA", 0);
+			testFrequencerWithRange("AAAB", "AAAAB", 0, 1, 3);
+			testFrequencerWithRange("AAAB", "AAAAB", 1, 2, 3);
+			testFrequencerWithRange("AAAB", "AAAAB", 1, 3, 2);
+			testFrequencerWithRange("AAAB", "AAAAB", 4, 5, 1);
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Exception occurred in Frequencer Object");
@@ -79,19 +94,26 @@ public class TestCase {
 			myObject = new InformationEstimator();
 			myObject.setSpace("3210321001230123".getBytes());
 			myObject.setTarget("0".getBytes());
+			System.err.println("---");
 			value = myObject.estimation();
 			assert (value > 1.9999) && (2.0001 > value)
 					: "IQ for 0 in 3210321001230123 should be 2.0. But it returns " + value;
 			myObject.setTarget("01".getBytes());
+
+			System.err.println("---");
 			value = myObject.estimation();
 			assert (value > 2.9999) && (3.0001 > value)
 					: "IQ for 01 in 3210321001230123 should be 3.0. But it returns " + value;
 			myObject.setTarget("0123".getBytes());
+
+			System.err.println("---");
 			value = myObject.estimation();
 			assert (value > 2.9999) && (3.0001 > value)
 					: "IQ for 0123 in 3210321001230123 should be 3.0. But it returns " + value;
 			myObject.setTarget("00".getBytes());
+			System.err.println("---");
 			value = myObject.estimation();
+
 			assert (value > 3.9999) && (4.0001 > value)
 					: "IQ for 00 in 3210321001230123 should be 3.0. But it returns " + value;
 			// spaceが未設定時
@@ -119,21 +141,7 @@ public class TestCase {
 			testEstimation("a", "", 0.0);// target length ==0
 			testEstimation("abc", "def", Double.MAX_VALUE);
 			// sub byteに対するテスト
-			testFrequencerWithRange("3210321001230123", "3210321001230123", 0, 16, 1);// 全範囲
-			testFrequencerWithRange("3210321001230123", "3210321001230123", 3, 4, 4);// 範囲が1
-			testFrequencerWithRange("3210321001230123", "3210321001230123", 3, 5, 1);// 範囲が2
-			testFrequencerWithRange("3210321001230123", "3210321001230123", 1, 3, 2);// 範囲が2
-			// slow/TestCaseから拝借
-			testFrequencer("AAA", "", -1);
-			testFrequencer("", "A", 0);
-			testFrequencer("AAA", "A", 3);
-			testFrequencer("AAA", "AA", 2);
-			testFrequencer("AAA", "AAA", 1);
-			testFrequencer("AAA", "AAAA", 0);
-			testFrequencerWithRange("AAAB", "AAAAB", 0, 1, 3);
-			testFrequencerWithRange("AAAB", "AAAAB", 1, 2, 3);
-			testFrequencerWithRange("AAAB", "AAAAB", 1, 3, 2);
-			testFrequencerWithRange("AAAB", "AAAAB", 4, 5, 1);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Exception occurred in InformationEstimator Object");
@@ -173,5 +181,33 @@ public class TestCase {
 		double iq = estimator.estimation();
 		assert (excepted - esp <= iq && iq <= excepted + esp)
 				: "IQ for " + target + " in " + space + " should be " + excepted + ". But it returns " + iq;
+	}
+
+	public static void testFrequencerWithDiff(String space, String target, FrequencerInterface test)
+			throws Exception {
+		FrequencerInterface executerBase = new Frequencer();
+
+		executerBase.setSpace(space.getBytes());
+		executerBase.setTarget(target.getBytes());
+		test.setSpace(space.getBytes());
+		test.setTarget(target.getBytes());
+		int freq = executerBase.frequency();
+		int res = test.frequency();
+		assert freq == res : space + ", " + target + ": " + res;
+
+	}
+
+	public static void testEstimationWithDiff(String space, String target, InformationEstimatorInterface test)
+			throws Exception {
+		double esp = 0.0001;// 許容誤差
+		InformationEstimatorInterface estimatorBase = new InformationEstimator();
+		estimatorBase.setSpace(space.getBytes());
+		estimatorBase.setTarget(target.getBytes());
+		test.setSpace(space.getBytes());
+		test.setTarget(target.getBytes());
+		double iq = estimatorBase.estimation();
+		double res = test.estimation();
+		assert (res - esp <= iq && iq <= res + esp)
+				: "IQ for " + target + " in " + space + " should be " + res + ". But it returns " + iq;
 	}
 }
